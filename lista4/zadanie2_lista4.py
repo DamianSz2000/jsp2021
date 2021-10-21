f07fb774aebfddfd3331709b4ee4bc2b445cadc3
DamianSz2000 <damianszmu.lik@gmail.com>
2021-10-21 04:30:48 +0200

    Update zadanie2_lista4.py

--- lista4/zadanie2_lista4.py
+++ lista4/zadanie2_lista4.py
@@ -1,5 +1,7 @@
 def usunPowt(t):
-    
+    t.sort()
+    t = list(dict.fromkeys(t))
+    return t
 
 n = int(input("Podaj ilosc elementow tablicy: "))
 print("Podawaj kolejno elementy tablicy: ")
@@ -7,3 +9,5 @@ tab = []
 for _ in range(0,n):
     elem = int(input())
     tab.append(elem)
+print("Lista: ", tab)
+print("Lista bez powtorzen: ", usunPowt(tab))
