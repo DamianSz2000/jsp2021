374e750b382bdef2ac0d14614291a52cd982e905
DamianSz2000 <damianszmu.lik@gmail.com>
2021-10-23 18:27:35 +0200

    Poprawki

--- lista1/zadanie5_lista1.py
+++ lista1/zadanie5_lista1.py
@@ -6,7 +6,7 @@ print(round(x))
 print(math.floor(x))
 """
 math.floor(x) zwraca najwieksza wartosc ktora jest mniejsza lub równa wartosci x
-round(x) dla liczb nieparzystych zaokragla od 0.5 w gore a ponizej w do, dla liczb parzystych zaokragla od 0.5 w dol powyzej w gore
+round(x) dla liczb z czescia calkowita nieparzysta zaokragla od 0.5 w gore a ponizej w do, dla liczb z czescia calkowita parzysta zaokragla od 0.5 w dol powyzej w gore
 operator // to po prostu dzielenie bez reszty
 
 """
--- lista1/zadanie9_lista1.py
+++ lista1/zadanie9_lista1.py
@@ -1,18 +1,11 @@
-import math
+import cmath
 z = complex(input("Podaj liczbe zespolona z: "))
-modul = math.sqrt(z.real**2 + z.imag**2)
+modul = cmath.sqrt(z.real**2 + z.imag**2)
 
-arg1 = z.real/modul
-arg2 = z.imag/modul
-if arg1 >= 0 and arg1 < 2*math.pi:
-    argmain = arg1
-else:
-    argmain = arg2
+argmain = cmath.phase(z)
 
 print("Liczba zespolona: ", z)
 print("Modul liczby zespolonej: ", modul)
 print("Liczba sprzezona z: ", z.conjugate())
 
-print("Argumentem 1 liczby zespolonej jest: ", arg1)
-print("Argumentem 2 liczby zespolonej jest: ", arg2)
 print("Argumentem glownym liczby zespolonej jest: ", argmain)
