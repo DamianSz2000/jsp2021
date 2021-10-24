d6f251a0326496ef58d7997eb8122e8bfdafab66
DamianSz2000 <damianszmu.lik@gmail.com>
2021-10-24 14:05:00 +0200

    Poprawki lista 2

--- lista2/zadanie10_lista2.py
+++ lista2/zadanie10_lista2.py
@@ -1,4 +1,5 @@
 a = list(range(3, 100, 3))
+print("Tablica poczatkowa: ", a)
 del a[4::3] #nie wiedzialem czy od 5 piatego wlacznie czy nie wiec zaczalem od 5 elementu WLACZNIE
 suma = sum(a)
 dl = len(a)
