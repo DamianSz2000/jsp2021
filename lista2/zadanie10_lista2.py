1ec748c5bf20bb7048bde8b9e52b330e2dd71e8d
DamianSz2000 <damianszmu.lik@gmail.com>
2021-10-27 12:03:49 +0200

    Poprawki

--- lista2/zadanie10_lista2.py
+++ lista2/zadanie10_lista2.py
@@ -1,7 +1,6 @@
+from statistics import mean
 a = list(range(3, 100, 3))
 print("Tablica poczatkowa: ", a)
 del a[4::3] #nie wiedzialem czy od 5 piatego wlacznie czy nie wiec zaczalem od 5 elementu WLACZNIE
-suma = sum(a)
-dl = len(a)
 print('Tablica po usunieciu co trzeciego elementu zaczynajac od piatego: ', a)
-print('Srednia arytmetyczna: ', suma/dl)
+print('Srednia arytmetyczna: ', mean(a))
