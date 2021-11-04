b51fc35d939396fe0b6204ccc39d67209748fa14
DamianSz2000 <damianszmu.lik@gmail.com>
2021-11-04 12:19:55 +0100

    Update zadanie5_lista3.py

--- lista3/zadanie5_lista3.py
+++ lista3/zadanie5_lista3.py
@@ -4,7 +4,8 @@ haslo = input("Wprowadz haslo aby sprawdzic jego sile: ")
 literaS = re.search(r"[a-z]", haslo) is None
 literaD = re.search(r"[A-Z]", haslo) is None
 literaSp = re.search(r"[#$@]", haslo) is None
-if literaS == False and literaD == False and literaSp == False and len(haslo) >= 6 and len(haslo) <= 16:
+literaCyfr = re.search(r"[0-9]", haslo) is None
+if literaS == False and literaD == False and literaSp == False and literaCyfr == False and len(haslo) >= 6 and len(haslo) <= 16:
     print("Haslo spelnia wymogi")
 else:
     print("Haslo nie spelnia wymogow")
