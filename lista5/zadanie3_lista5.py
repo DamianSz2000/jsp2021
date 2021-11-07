f9711c9b457181cd7e7d634ec38f6cdefebb20ef
DamianSz2000 <damianszmu.lik@gmail.com>
2021-11-07 19:56:27 +0100

    Ukończenie zadania 3 lista 5

--- lista5/zadanie3_lista5.py
+++ lista5/zadanie3_lista5.py
@@ -1,30 +1,30 @@
-roman = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-}
-
-def roman_to_arabic(x):
+def roman_to_arab(roman):
+    roman_signs = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     converted_number = 0
-    temp = 0
-    if (x.count("V") > 1 or x.count("L") > 1 or x.count("D") > 1) or ((x.count("I") > 3 or x.count("X") > 3 or x.count("C") > 3 or x.count("M") > 3)):
-        return "Nie mozna powtarzac znakow V L i D! Nie mozna powtarzac znakow I X C i M wiecej niz 3 razy!" 
-    else:
-        for char in range(len(x)-1, 0, -1):
-            if roman[x[char]] <= roman[x[char-1]]:
-                converted_number += roman[x[char]]
-                print(converted_number)
-                if char == 1:
-                    converted_number += roman[x[char-1]]
-            elif roman[x[char]] >= roman[x[char-1]]:
-                converted_number += roman[x[char]] - roman[x[char-1]]
-                print(converted_number)
+    length = len(roman)
+    i = 0
+    while i < length-1:
+        if roman_signs[roman[i]] >= roman_signs[roman[i+1]]:
+            converted_number += roman_signs[roman[i]]
+            i += 1
+        elif roman_signs[roman[i]] < roman_signs[roman[i+1]]:
+            converted_number += roman_signs[roman[i+1]] - roman_signs[roman[i]]
+            i += 2
+    if roman_signs[roman[length-1]] <= roman_signs[roman[length-2]]:
+        converted_number += roman_signs[roman[length-1]]
         return converted_number
-
-
-x = input("Podaj liczbe w formacie rzymskim aby przetworzyc ja na liczbe arabska: ")
-print("Liczba po konwersji: ", roman_to_arabic(x))
+    else:
+        return converted_number
+roman = input("Podaj liczbe rzymska aby zmienic ja na arabska: ")
+if (roman.count("L") > 1 or roman.count("V") > 1 or roman.count("D") > 1) or (roman.count("XXXX") == 1 or roman.count("IIII") == 1 or roman.count("CCCC") == 1 or roman.count("MMMM") == 1):
+    print("Litery L, V oraz D nie moga sie powtarzac! Litery M, C, X oraz I nie moga byc obok siebie wiecej niz 3 razy np XXXX jest zapisem blednym!")
+else:
+    print("Liczba",roman,"w systemie arabskim to",roman_to_arab(roman))
